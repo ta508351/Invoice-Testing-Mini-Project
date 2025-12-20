c4f06e16bd98aa19171c102ef0d3b4a8c4e151c2
Tanya Agrawal <ta508351@gmail.com>
2025-12-20 20:51:32 +0530

    Add page counting and credit calculation using PyPDF2

--- invoice_testing/scripts/page_counter.py
+++ invoice_testing/scripts/page_counter.py
@@ -1,26 +1,29 @@
 from PyPDF2 import PdfReader
 from pathlib import Path
 invoices_path = Path("invoice_testing/invoices")
-results ={}
-try:   
-    for invoice in invoices_path.iterdir():
-        try:
-            if  invoice.is_file() and invoice.suffix == ".pdf":
-                with invoice.open("rb") as file:
-                    reader = PdfReader(file)
-                    page_count = len(reader.pages)
-                    credits = page_count
-                    results[invoice.name] = {
+def counting_page():
+    result ={}
+    try:   
+        for invoice in invoices_path.iterdir():
+            try:
+                if  invoice.is_file() and invoice.suffix == ".pdf":
+                    with invoice.open("rb") as file:
+                        reader = PdfReader(file)
+                        page_count = len(reader.pages)
+                        credits = page_count
+                        result[invoice.name] = {
                                               "pages": page_count,
                                               "credits": credits
                                             }
-        except IOError:
-            print("File can't be opened: invoice_name")
-       
-    print(results)        
-            
-except FileNotFoundError:
-    print("Invalid Folder")        
+                    
+            except IOError:
+                print("File can't be opened: invoice_name")
+    except FileNotFoundError:
+        print("Invalid Folder") 
+    return result  
+if __name__ == "__main__":
+    result = counting_page()
+    print(result)         
     
         
 
